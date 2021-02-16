9a23c20c671aa5663e1321e93fd3b5c93ce1d7fd
Irfan Zainudin <m.irfan.zain@gmail.com>
2021-02-16 16:00:34 +1100

    completed refactoring of Header and Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Collapsible from 'react-collapsible';
-import Typewriter from 'typewriter-effect';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBars,
@@ -16,10 +15,7 @@ import './Navbar.css';
 
 export default function Navbar({ parentClassName }) {
   return (
-    <header className={parentClassName}>
-      <h1 className="mobile-only">
-        <img src={logo} className="logo" alt="Official MASCA NSW Logo" />
-      </h1>
+    <div id="navbars">
       <div className="navbar desktop-only">
         <Link to="/">
           <img src={logo} className="logo" alt="Official MASCA NSW Logo" />
@@ -61,109 +57,97 @@ export default function Navbar({ parentClassName }) {
         <Link to="/affiliates-sponsors"><p>Affiliates & Sponsors</p></Link>
         <button className="dark-mode-button"><FontAwesomeIcon icon={faMoon}/></button>
       </div>
-      <div className="greetings-container">
-        <span className="greetings">
-          <Typewriter
-            options={{
-              strings: ['Hello', 'Hai', '你好', 'Vanakkam'],
-              autoStart: true,
-              loop: true
-            }}
-          />
-        </span>
-        <span className="masca-full-name">
-          MALAYSIAN STUDENTS'<br/>COUNCIL OF AUSTRALIA
-        </span>
-        <h3 className="energetic-yellow">
-          New South Wales Chapter
-        </h3>
-      </div>
-      <h1 className="mobile-only">
-        <button onClick={() => {
-          document.getElementById("mobile-navbar-content").classList.add("show");
-        }}>
-          <FontAwesomeIcon icon={faBars} />
-        </button>
-      </h1>
-      <div id="mobile-navbar-content" className="mobile-only">
-        <h1 className="close-mobile-navbar">
-          <button className="dark-blue" onClick={() => {
-            document.getElementById("mobile-navbar-content").classList.remove("show");
+      <div className="mobile-navbar mobile-only">
+        <h1>
+          <img src={logo} className="logo" alt="Official MASCA NSW Logo" />
+        </h1>
+        <h1>
+          <button onClick={() => {
+            document.getElementById("mobile-navbar-content").classList.add("show");
           }}>
-            <FontAwesomeIcon icon={faTimes} />
+            <FontAwesomeIcon icon={faBars} />
           </button>
         </h1>
-        <Collapsible
-          trigger={
-            <p>
-              About <FontAwesomeIcon icon={faSortDown} className="margined-bottom" />
-            </p>
-          }
-        >
-          <Link to="/about">
-            <p>Current Council</p>
+        <div id="mobile-navbar-content">
+          <h1 className="close-mobile-navbar">
+            <button className="dark-blue" onClick={() => {
+              document.getElementById("mobile-navbar-content").classList.remove("show");
+            }}>
+              <FontAwesomeIcon icon={faTimes} />
+            </button>
+          </h1>
+          <Collapsible
+            trigger={
+              <p>
+                About <FontAwesomeIcon icon={faSortDown} className="margined-bottom" />
+              </p>
+            }
+          >
+            <Link to="/about">
+              <p>Current Council</p>
+            </Link>
+            <Link to="/past-council">
+              <p>Past Council</p>
+            </Link>
+          </Collapsible>
+          <Link to="/events">
+            <p>Events</p>
           </Link>
-          <Link to="/past-council">
-            <p>Past Council</p>
+          <Collapsible
+            trigger={
+              <p>
+                Initiatives <FontAwesomeIcon icon={faSortDown} className="margined-bottom" />
+              </p>
+            }
+          >
+            <Link to="/initiatives">
+              <p>All Initiatives</p>
+            </Link>
+            <Link to="/welfare-initiatives">
+              <p>Student Welfare</p>
+            </Link>
+            <Link to="/careers-initiatives">
+              <p>Careers & Development</p>
+            </Link>
+            <Link to="/cultural-initiatives">
+              <p>Cultural</p>
+            </Link>
+            <Link to="/sports-initiatives">
+              <p>Sports</p>
+            </Link>
+            <Link to="/officers-program">
+              <p>Officers Program</p>
+            </Link>
+            <Link to="/student-discounts">
+              <p>Student Discounts</p>
+            </Link>
+          </Collapsible>
+          <Collapsible
+            trigger={
+              <p>
+                Student Guide <FontAwesomeIcon icon={faSortDown} className="margined-bottom" /> 
+              </p>
+            }
+          >
+            <Link to="/student-guide">
+              <p>All Guides</p>
+            </Link>
+            <Link to="/starter-kit">
+              <p>Australia Starter Kit</p>
+            </Link>
+            <Link to="/jalan-jalan">
+              <p>Jalan-Jalan</p>
+            </Link>
+            <Link to="/cari-makan">
+              <p>Cari Makan</p>
+            </Link>
+          </Collapsible>
+          <Link to="/affiliates-sponsors">
+            <p>Affiliates and Sponsors</p>
           </Link>
-        </Collapsible>
-        <Link to="/events">
-          <p>Events</p>
-        </Link>
-        <Collapsible
-          trigger={
-            <p>
-              Initiatives <FontAwesomeIcon icon={faSortDown} className="margined-bottom" />
-            </p>
-          }
-        >
-          <Link to="/initiatives">
-            <p>All Initiatives</p>
-          </Link>
-          <Link to="/welfare-initiatives">
-            <p>Student Welfare</p>
-          </Link>
-          <Link to="/careers-initiatives">
-            <p>Careers & Development</p>
-          </Link>
-          <Link to="/cultural-initiatives">
-            <p>Cultural</p>
-          </Link>
-          <Link to="/sports-initiatives">
-            <p>Sports</p>
-          </Link>
-          <Link to="/officers-program">
-            <p>Officers Program</p>
-          </Link>
-          <Link to="/student-discounts">
-            <p>Student Discounts</p>
-          </Link>
-        </Collapsible>
-        <Collapsible
-          trigger={
-            <p>
-              Student Guide <FontAwesomeIcon icon={faSortDown} className="margined-bottom" /> 
-            </p>
-          }
-        >
-          <Link to="/student-guide">
-            <p>All Guides</p>
-          </Link>
-          <Link to="/starter-kit">
-            <p>Australia Starter Kit</p>
-          </Link>
-          <Link to="/jalan-jalan">
-            <p>Jalan-Jalan</p>
-          </Link>
-          <Link to="/cari-makan">
-            <p>Cari Makan</p>
-          </Link>
-        </Collapsible>
-        <Link to="/affiliates-sponsors">
-          <p>Affiliates and Sponsors</p>
-        </Link>
-        <p><button className="dark-mode-button"><FontAwesomeIcon icon={faMoon}/></button></p>
+          <p><button className="dark-mode-button"><FontAwesomeIcon icon={faMoon}/></button></p>
+        </div>
       </div>
-    </header>
+    </div>
   )
 }
